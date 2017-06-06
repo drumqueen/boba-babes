45f53f3b3587ea3bdeb67968e3142176a099f323
Doreen Hoang <doreen@stripe.com>
2017-06-05 18:58:03 -0700

    create database

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ get '/' do
   "This is the home page."
 end
 
-
 get '/success' do
   "Your payment has processed successfully."
+end
+
+post '/pay' do
+  puts "The data sent to the /pay POST route is:"
+  p params
+  redirect '/success'
 end
