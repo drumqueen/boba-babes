647e90c2b7993425c4459f26f23b32d26aa81716
Doreen Hoang <doreen@stripe.com>
2017-06-07 18:09:04 -0700

    connect database

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
+require 'sqlite3'
 
 get '/' do
   "This is the home page."
+
+  # connect to the database
+  db = SQLite3::Database.open('store.db')
+
+  # configure results to be returned as as an array of hashes instead of nested arrays
+  db.results_as_hash = true
+
+  # query the products table and print the result
+  puts "Database query results:"
+  p db.execute("SELECT id, description, price FROM products;")
+
+  # close database connection
+  db.close
+
   erb :home
 end
 
